20bef922ab70d50d5c21525d651b5c362e6a0cd5
jordanfogou <jordan.fogou@viacesi.fr>
2023-11-26 10:26:20 +0100

    message

--- testgit.cpp
+++ testgit.cpp
@@ -4,4 +4,13 @@ public: @
 	std::cout << " ceci est un test " << std:endl;
 };
 
-	bon la j'ecris du n'importe quoi questio de tester git mon git add puis mon git commit von foncionner 
+	bon la j'ecris du n'importe quoi questio de tester git mon git add puis mon git commit vogitn foncion
+		
+		
+		
+		
+		garss
+
+		
+		
+		bon la je fous le bordel pour voir si ca marche vraiment 
